59c77e94caa0b094e38d5d77d00ff27efd1c5a23
Ishola Mujeeb <isholamujeebadekunle@gmail.com>
2025-01-14 19:59:50 +0100

    BREAKING CHANGES: added keeper.html

--- scripts/secondkeeper.js
+++ scripts/secondkeeper.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const inputArea = document.querySelector('.js-title-box')
 const plusSign = document.querySelector('.js-plus-sign')
 const todoGrid = document.querySelector('.js-todo-grid')
@@ -76,4 +77,84 @@ deleteTodo()
 function saveToStorage(){
   localStorage.setItem('todos', JSON.stringify(todoArray))
 }
+=======
+const inputArea = document.querySelector('.js-title-box')
+const plusSign = document.querySelector('.js-plus-sign')
+const todoGrid = document.querySelector('.js-todo-grid')
+const textArea = document.querySelector('.js-textarea')
+
+const todoArray = JSON.parse(localStorage.getItem('todos')) || [];
+
+function renderTodo(){
+let html = '';
+todoArray.forEach((todo,index)=>{
+  html += `
+ <div class="todo" data-index = "${index}">
+      <h1>${todo.arrayInput}</h1>
+      <p>${todo.arrayPlaceholder}</p>
+      <button class="delete-button js-delete-button">
+      <i class="fas fa-trash"></i> Delete
+      </button>
+      </div>
+`
+})
+todoGrid.innerHTML = html;
+deleteTodo()
+};
+
+
+renderTodo()
+
+function generateFullTextArea(){
+  textArea.addEventListener('click',()=>{
+    inputArea.style.display = 'block';
+    const lines = textArea.value.split('\n').length;
+    textArea.rows = Math.max(lines, 3);
+    plusSign.style.display = 'block';
+  })
+}
+
+generateFullTextArea()
+
+function addTodo(){
+  plusSign.addEventListener('click', ()=>{
+    const title = inputArea.value;
+    const description = textArea.value;
+    if(title === '' || description===''){
+      alert('Please fill in both fields:')
+      return;
+    }
+    todoArray.push({
+      arrayInput: title,
+      arrayPlaceholder: description
+    })
+    renderTodo()
+    saveToStorage()
+    inputArea.value = '';
+    textArea.value = '';
+  })
+}
+addTodo()
+
+
+function deleteTodo(){
+  const deleteButtons = document.querySelectorAll('.js-delete-button')
+  deleteButtons.forEach((deleteButton)=>{
+    deleteButton.addEventListener('click', (event)=>{
+        const todoElement = event.target.closest('.todo')
+        const index = Number(todoElement.dataset.index);
+        todoArray.splice(index, 1);
+        renderTodo()
+        saveToStorage()   
+    })
+  })
+
+}
+
+
+deleteTodo()
+function saveToStorage(){
+  localStorage.setItem('todos', JSON.stringify(todoArray))
+}
+>>>>>>> 37da62f (main)
 saveToStorage()
